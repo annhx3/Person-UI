ab07227e701f8c25e7b70c458c2277295d670a12
Alyssa Nielsen <alyssanielsen@alyssas-mbp.centurylink.net>
2017-06-28 20:03:23 -0500

    Created Person UI project

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Alyssa Nielsen on 6/28/17.
+//  Copyright © 2017 Alyssa Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
